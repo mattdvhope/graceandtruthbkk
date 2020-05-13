b3fec051ad4bb57a13f57623fd24d6d6e837238f
mattdvhope <mattagape@me.com>
2020-05-13 16:47:17 +0700

    Commented out

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { rhythm } from '../utils/typography'
+// import React from 'react'
+// import { rhythm } from '../utils/typography'
 
-export default ({ settings }) => (
-  <div
-    style={{
-      display: 'flex',
-      marginTop: rhythm(2),
-    }}
-  >
-    <img
-      src={settings.author_avatar.imgix_url}
-      alt={settings.author_name}
-      style={{
-        marginRight: rhythm(1 / 2),
-        marginBottom: 0,
-        width: rhythm(2),
-        height: rhythm(2),
-        borderRadius: `50%`,
-      }}
-    />
-    <div style={{ fontSize: `140%` }} dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
-  </div>
-)
+// export default ({ settings }) => (
+//   <div
+//     style={{
+//       display: 'flex',
+//       marginTop: rhythm(2),
+//     }}
+//   >
+//     <img
+//       src={settings.author_avatar.imgix_url}
+//       alt={settings.author_name}
+//       style={{
+//         marginRight: rhythm(1 / 2),
+//         marginBottom: 0,
+//         width: rhythm(2),
+//         height: rhythm(2),
+//         borderRadius: `50%`,
+//       }}
+//     />
+//     <div style={{ fontSize: `140%` }} dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
+//   </div>
+// )
