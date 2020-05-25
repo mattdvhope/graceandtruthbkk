6815d12f405e254cd5884b7bc07ef95de5dd3b0d
mattdvhope <mattagape@me.com>
2020-05-25 23:30:35 +0700

    Changed url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -26,7 +26,7 @@ export const logout = callback => {
 }
 
 export async function addVisit(name, picture) {
-  const saved_person = await fetch(`http://localhost:3000/users`, {
+  const saved_person = await fetch(`https://graphql-rails-pg1.herokuapp.com/users`, {
     method: 'POST',
     headers: {'Content-Type':'application/x-www-form-urlencoded'},
     body: `name=${name}&picture=${picture}`
