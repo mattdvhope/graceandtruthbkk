7138583e163f13c2012d2fa227eb3ea950682e56
mattdvhope <mattagape@me.com>
2020-05-26 00:21:52 +0700

    Refactored

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -27,6 +27,7 @@ export const logout = callback => {
 
 export async function addVisit(name, picture) {
   const saved_person = await fetch(`https://graphql-rails-pg1.herokuapp.com/users`, {
+  // const saved_person = await fetch(`http://localhost:3000/users`, {
     method: 'POST',
     headers: {'Content-Type':'application/x-www-form-urlencoded'},
     body: `name=${name}&picture=${picture}`
@@ -34,6 +35,14 @@ export async function addVisit(name, picture) {
   const saved_person_data = await saved_person.json()
   sessionStorage.setItem("user_data", JSON.stringify(saved_person_data))
 }
+
+export const linkVisit = () => {
+  const user_data = JSON.parse(sessionStorage.getItem("user_data"));
+  let visits = user_data.user.visits
+  addVisit(user_data.user.name, user_data.user.picture)
+  console.log(visits + 1)
+}
+
 
 
 // see 'https://www.gatsbyjs.org/docs/authentication-tutorial/'
