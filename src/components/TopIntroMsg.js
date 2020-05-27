3e61799d026a0c9e280976612548c5ab18ccf248
mattdvhope <mattagape@me.com>
2020-05-27 15:36:22 +0700

    edited intro for logged in

--- src/components/TopIntroMsg.js
+++ src/components/TopIntroMsg.js
@@ -10,9 +10,9 @@ const TopIntroMsg = ({ description, width }) => (
         fontSize: `130%`,
       }}
     >
-    <hr style={{ marginBottom: `0%`, width: `13em` }}/>
-    {description}
-    <hr style={{ marginBottom: `0%`, width: `13em` }}/>
+    <hr style={{ marginBottom: `0%` }}/>
+    <div style={{ marginLeft: `auto`, marginRight: `auto` }}>{description}</div>
+    <hr style={{ marginBottom: `0%` }}/>
   </div>
 
 )
